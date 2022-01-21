6790c6cd324d93edcdba2047860f969df5ca111c
王冲 <chongwang27@creditease.cn>
2022-01-21 10:49:37 +0800

    iOS 15 的简单适配

--- Example/JKPageView/AppDelegate.swift
+++ Example/JKPageView/AppDelegate.swift
@@ -15,6 +15,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        if #available(iOS 15.0, *) {
+            let app = UINavigationBarAppearance()
+            // 重置背景和阴影颜色
+            app.configureWithOpaqueBackground()
+            app.titleTextAttributes = [
+                NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18),
+                NSAttributedString.Key.foregroundColor: UIColor.brown
+            ]
+            app.backgroundColor = UIColor.white// 设置导航栏背景色
+            app.shadowColor = .clear
+            app.backgroundEffect = nil
+            // 带scroll滑动的页面
+            UINavigationBar.appearance().scrollEdgeAppearance = app
+            // 常规页面，描述导航栏以标准高度
+            UINavigationBar.appearance().standardAppearance = app
+        }
+        
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.rootViewController = UINavigationController(rootViewController: ViewController())
         self.window?.makeKeyAndVisible()
